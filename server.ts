73f9e7a195eb9794abb3f6f554814250b317d5a1
unknown <wallacetashyn@gmail.com>
2022-07-05 12:04:14 -0500

    server listen fix

--- server.ts
+++ server.ts
@@ -28,4 +28,4 @@ app.all('*', (req: Request, res: Response) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT || PORT, () => console.log(`Server running on port ${PORT}`));
