2ba1a0b8e63ede7b51f041f1bfb2f147fcd90bdd
unknown <wallacetashyn@gmail.com>
2022-08-27 15:39:13 -0500

    run crawling function every 2 hours

--- server.ts
+++ server.ts
@@ -47,7 +47,7 @@ app.all('*', (req: Request, res: Response) => {
 
 app.listen(process.env.PORT || PORT, () => console.log(`Server running on port ${PORT}`));
 
-cron.schedule('0 */1 * * *', async function() {
+cron.schedule('0 */2 * * *', async function() {
     await updateArticleResults()
     console.log(`Articles fetched at ${new Date().toISOString()}`)
 });
