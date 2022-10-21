36473626d4889d1ff5449bda0d2757f8798746d5
unknown <wallacetashyn@gmail.com>
2022-10-21 00:21:47 -0500

    cyclic temp config

--- server.ts
+++ server.ts
@@ -15,35 +15,41 @@ const databaseEnviroment = process.env.DATABASE_URI || '';
 //connect to DB
 const connectDB=async()=>{
     try{
-        await mongoose.connect(databaseEnviroment).then(()=>{
-            // built-in middleware to handle urlencoded form data
-            app.use(express.urlencoded({ extended: false }));
-
-            // built-in middleware for json 
-            app.use(express.json());
-
-            //middleware for cookies
-            app.use(cookieParser());
-
-            app.use('/',cors() ,require('./routes/main'));
-
-            app.all('*', (req: Request, res: Response) => {
-            res.status(404);
-            if (req.accepts('html')) {
-                res.sendFile(path.join(__dirname, 'views', '404.html'));
-            } else if (req.accepts('json')) {
-                res.json({ "error": "404 Not Found" });
-            } else {
-                res.type('txt').send("404 Not Found");
-            }
-            });
-        });
+        await mongoose.connect(databaseEnviroment);
         console.log('Connected to MongoDB!')
     }catch(err){
         console.error(err);
     }
 }
-connectDB();
+connectDB().then(()=>{
+    app.use(express.urlencoded({ extended: false }));
+
+    // built-in middleware for json 
+    app.use(express.json());
+
+    //middleware for cookies
+    app.use(cookieParser());
+
+    app.use('/',cors() ,require('./routes/main'));
+
+    app.all('*', (req: Request, res: Response) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+    });
+
+    app.listen(process.env.PORT || PORT, () => console.log(`Server running on port ${PORT}`));
+
+    cron.schedule('0 */2 * * *', async function() {
+        await updateArticleResults()
+        console.log(`Articles fetched at ${new Date().toISOString()}`)
+    });
+});
 
 // built-in middleware to handle urlencoded form data
 /*
@@ -66,11 +72,12 @@ app.all('*', (req: Request, res: Response) => {
   } else {
       res.type('txt').send("404 Not Found");
   }
-});*/
+});
 
 app.listen(process.env.PORT || PORT, () => console.log(`Server running on port ${PORT}`));
 
-cron.schedule('0 */2 * * *', async function() {
+cron.schedule('0 *//*2 * * *', async function() {
     await updateArticleResults()
     console.log(`Articles fetched at ${new Date().toISOString()}`)
 });
+*/
